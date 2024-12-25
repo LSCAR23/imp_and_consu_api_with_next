f99886a51abd7138c12f3768a0507407eed1afd5
LSCAR23 <laraoscar2310@gmail.com>
2024-12-25 13:28:19 -0600

    optimizing the api file

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export const fetchPosts = async () => {
     const response = await axios.get(API_URL);
@@ -8,12 +8,27 @@ export const fetchPosts = async () => {
 };
 
 export const createPost = async (post) => {
-    const response = await axios.post(API_URL, post);
+    const response = await axios.post(API_URL, {
+        title: post.title,
+        body: post.body,
+        userId: 1
+    }, {
+        headers: {
+            'Content-Type': 'application/json; charset=UTF-8',
+        },
+    });
     return response.data;
 };
 
 export const updatePost = async (post) => {
-    const response = await axios.patch(`${API_URL}/${post.id}`, post);
+    const response = await axios.patch(`${API_URL}/${post.id}`, {
+        title: post.title,
+        body: post.body,
+    }, {
+        headers: {
+            'Content-Type': 'application/json; charset=UTF-8',
+        },
+    });
     return response.data;
 };
 
